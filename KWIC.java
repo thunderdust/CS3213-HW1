d05b42c43b73743005bb895d2970d7d5bd8c27ee
Liu Weiran <liuweiran.nus@gmail.com>
2013-08-28 17:24:11 +0800

    initial commit

---
+++ KWIC.java
@@ -0,0 +1,32 @@
+
+public class KWIC {
+
+	
+	public static String [] IGNORE_LIST;
+	// text UI to give guide to users
+	private static String [] USER_GUIDE = {
+		"Please enter the title, press ENTER to finish: ","The keywords are listed below: "
+	};
+	
+	
+	public String getInput (){
+		return null;
+		
+	}
+	
+	public String circularShift() {
+		return null;
+		
+	}
+	
+	public void displayLines() {
+		
+	}
+	
+	private String[] sortLines() {
+		return null;
+		
+	}
+	
+	
+}
